4bc08f1e66c4aa94534acaa7d737f64539ed4f9c
Pr5556 <princprize@gmail.com>
2025-06-02 14:15:02 +0530

    Added author's Prince

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class helloWorld {
     public static void main(String[]args){
         System.out.println("Hello from GitHub and Java");
+         System.out.println("Hello from GitHub and Java");
     }
 }
